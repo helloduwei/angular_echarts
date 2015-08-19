3d171d9d3963710af8252cda8928098f98496787
duwei <helloduwei@hotmail.com>
2015-08-19 15:25:20 +0800

    add bar module+data,add loading,add device turn action

--- js/controllers/mainController.js
+++ js/controllers/mainController.js
@@ -1,111 +1,125 @@
 
 mainApp.controller('main', function ($scope,$rootScope,$route,$window,$routeParams,dataContent) {
-	var datas = [];
-	var ar_date = [];
-	var ar_k = [];
+	$scope.loading = true;
+	//refresh when device turn
+	window.onorientationchange = function(){
+		window.location.reload(true);
+}
+	//fetch data
+	var datas = [],
+		ar_date = [],
+		ar_k = [],
+		ar_num = [];
+	var _code = '';
 	dataContent.getData()
 	.success(function (data){
-		
 		datas = data.Table;
 		//rebuild array from data
+		_code = datas[0].证券代码;
 		for(var i = 0;i<datas.length;i++){
 			ar_date.push(datas[i].交易日期);
+			ar_num.push((datas[i].成交量)/100000000);
 			var ar_holder = [];
-			ar_holder.push(datas[i].开盘价,datas[i].收盘价,datas[i].最低价,datas[i].最高价);
+			ar_holder.push(parseFloat(datas[i].开盘价),parseFloat(datas[i].收盘价),parseFloat(datas[i].最低价),parseFloat(datas[i].最高价));
 			ar_k.push(ar_holder);
 		}
-		/*console.log(ar_date);
-		console.log(ar_k);*/
-		loadEcharts(ar_date,ar_k);
-
+		loadEcharts(_code,ar_date,ar_k,ar_num);
+		$scope.loading = false;
 	})
 	.error(function (err){
 		console.log(err)
 	})
 
-function loadEcharts(dataX,dataY){
-	require.config({
-		paths:{
-			echarts:'http://echarts.baidu.com/build/dist'
-		}
-	});
+	//require echarts
+	function loadEcharts(title,dataX,data_k,data_num){
+		require.config({
+			paths:{
+				echarts:'http://echarts.baidu.com/build/dist'
+			}
+		});
 
-	require(
-	[
-		'echarts',
-		'echarts/chart/k',
-	],
-	function (ec){
-		var myChart = ec.init(document.getElementById('main'));
-		
-		option = {
-			backgroundColor:'#fff',
-			color:'#730000',
-			calculable:true,
-		    title : {
-		        text: '动态数据',
-		        /*subtext: '纯属虚构'*/
-		    },
-		    tooltip : {
-		        trigger: 'item',
-		    },
-		    legend: {
-		    	show: false,
-		        data:['上证指数', /*'随机数据'*/]
-		    },
-		    toolbox: {
-		        show : false,
-		        feature : {
-		            mark : {show: true},
-		            dataView : {show: true, readOnly: false},
-		            restore : {show: true},
-		            saveAsImage : {show: true}
-		        }
-		    },
-		    dataZoom : {
-		        show : true,
-		        realtime: true,
-		        start : 0,
-		        end : 10
-		    },
-		    xAxis : [
-		        {
-		            type : 'category',
-		            boundaryGap : true,
-		            data : dataX
-		        },
-		        {
-		            type : 'value',
-		            max:100,
-		            scale: true
-		        }
-		    ],
-		    yAxis : [
-		        {
-		            type : 'value',
-		            scale:true,
-		            splitNumber: 5,
-		            boundaryGap: [0.05, 0.05]
-		        },
-		        {
-		            type : 'value',
-		            splitNumber: 5,
-		            scale: true
-		        }
-		    ],
-		    series : [
-		        {
-		            name:'上证指数',
-		            type:'k',
-		            data:dataY
-		        }
-		    ]
-	};
-		myChart.setOption(option);
-		myChart.setTheme('macarons'); //macarons,infographic
+		require(
+		[
+			'echarts',
+			'echarts/chart/bar',
+			'echarts/chart/k',
+		],
+		function (ec){
+			var myChart = ec.init(document.getElementById('main'));
+			
+			option = {
+				calculable:false,
+			    title : {
+			        text: title,
+			        x:'center',
+			        y:'top'
+			        /*subtext: '纯属虚构'*/
+			    },
+			    tooltip : {
+			        trigger: 'item',
+			    },
+			    legend: {
+			    	show: true,
+			        data:['k线( 开/收/低/高 )', '成交量( /亿 )'],
+			        x:'center',
+			        y:35
+			    },
+			    toolbox: {
+			        show : false,
+			        feature : {
+			            mark : {show: true},
+			            dataView : {show: true, readOnly: false},
+			            restore : {show: true},
+			            saveAsImage : {show: true}
+			        }
+			    },
+			    dataZoom : {
+			        show : true,
+			        realtime: true,
+			        start : 50,
+			        end : 100
+			    },
+			    xAxis : [
+			        {
+			            type : 'category',
+			            boundaryGap : true,
+			            data : dataX
+			        }
+			    ],
+			    yAxis : [
+			        {
+			            type : 'value',
+			            scale:true,
+			            min:4,
+			            splitNumber: 5,
+			            boundaryGap: [0.05, 0.05]
+			        },
+			        {
+			            type : 'value',
+			            scale: true,
+			            min:0,
+			            max:20,
+			            boundaryGap: [0.05, 0.05] 
+			        }
+			    ],
+			    series : [
+			        {
+			            name:'k线( 开/收/低/高 )',
+			            type:'k',
+			            data:data_k
+			        },{
+			            name:'成交量( /亿 )',
+			            type:'bar',
+			            yAxisIndex:1,
+			            data:data_num
+			        }
+			    ]
+		};
+			myChart.setOption(option);
+			myChart.setTheme('macarons'); //macarons,infographic
+		}
+		);
 	}
-	);
-}
 
 });
 
